5bc2889170072a60e09caa744a8884979be062dd
wiktorsachar <44320437+wiktorsachar@users.noreply.github.com>
2019-02-18 14:33:44 +0100

    Update Unchecker.js

--- ReactExam/src/Components/Catalog/Components/Unchecker/Unchecker.js
+++ ReactExam/src/Components/Catalog/Components/Unchecker/Unchecker.js
@@ -2,6 +2,7 @@ import ManufactureSorter from '../ManufactureSorter/ManufactureSorter';
 
 const Unchecker = () => {
     let filtered = ManufactureSorter();
+    filtered.push("all");
     for (let i = 0; i < filtered.length; i++) {
         document.getElementById(filtered[i]).checked = false;
         
